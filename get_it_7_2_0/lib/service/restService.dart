fd4d396fe7057f09919b528a65a16218e84bff0e
QuocBao <bao.bui@fastboy.net>
2022-02-16 22:46:14 +0700

    Delete restService.dart

--- get_it_7_2_0/lib/service/restService.dart
+++
@@ -1,8 +0,0 @@
-class RestService {
-  Future<RestService> init() async {
-    // do your async initialisation...
-    // simulating it with a Delay here
-    await Future.delayed(Duration(seconds: 2));
-    return this;
-  }
-}
