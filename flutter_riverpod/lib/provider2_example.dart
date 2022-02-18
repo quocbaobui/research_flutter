3fa2f3b6e2697f7710a4ffcd1124e7e4aa51af32
QuocBao <bao.bui@fastboy.net>
2022-02-18 19:57:22 +0700

    config android
    + ext.kotlin_version = '1.5.10'
    +  compileSdkVersion 30 -> 31

--- flutter_riverpod/lib/provider2_example.dart
+++ flutter_riverpod/lib/provider2_example.dart
@@ -29,3 +29,28 @@ class MyAppProviderEx2 extends StatelessWidget {
     );
   }
 }
+
+
+//   @override
+//   Widget build(BuildContext context1) {
+//     return ChangeNotifierProvider<ClassTestAChangeNotifer>(
+//       create: (context3) => ClassTestAChangeNotifer(),
+//       child: Consumer<ClassTestAChangeNotifer>(
+//           builder: (context4, classTestA, child) {
+//         debugPrint(" Rebuild MyAppProviderEx2 ");
+//         return Scaffold(
+//           appBar: AppBar(title: Text(" Provider MyAppProviderEx2")),
+//           body: Center(
+//               child: Text(
+//                   "Your name: ${context4.read<ClassTestAChangeNotifer>().getName}")),
+//           floatingActionButton: FloatingActionButton(
+//             child: Text("Name"),
+//             onPressed: () {
+//               classTestA.setName("Nguyen van Toan");
+//             },
+//           ),
+//         );
+//       }),
+//     );
+//   }
+// }
