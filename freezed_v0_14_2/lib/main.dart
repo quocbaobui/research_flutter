3fe82759051d56ab1b28daae833c38b2443480fe
Bui Quoc Bao <99718101+quocbaobui@users.noreply.github.com>
2022-02-23 16:27:08 +0700

    update example freezed

--- freezed_v0_14_2/lib/main.dart
+++ freezed_v0_14_2/lib/main.dart
@@ -4,28 +4,47 @@ import 'package:freezed_v0_14_2/models/user_internal.dart';
 import 'models_freezed/user_internal_w.dart';
 
 void main() {
-  UserInternal userInternal1 = UserInternal(userId: 0, name: "Bao Test1");
-  UserInternal userInternal2 = UserInternal(userId: 0, name: "Bao Test1");
-  UserInternal userInternal3 = UserInternal(userId: 0, name: "Bao Test2");
+  UserInternal userInternal1 =
+      UserInternal(userId: 0, name: "Bao Test1", age: 8);
+  UserInternal userInternal2 =
+      UserInternal(userId: 0, name: "Bao Test1", age: 8);
+  UserInternal userInternal3 =
+      UserInternal(userId: 1, name: "Bao Test2", age: 8);
 
-  UserInternalWF userInternalWF1 = UserInternalWF(userId: 0, name: "Bao Test1");
-  UserInternalWF userInternalWF2 = UserInternalWF(userId: 1, name: "Bao Test1");
-  UserInternalWF userInternalWF3 = UserInternalWF(userId: 0, name: "Bao Test2");
+  UserInternalWF userInternalWF1 =
+      UserInternalWF(userId: 0, name: "Bao Test1", age: 8);
+  UserInternalWF userInternalWF2 =
+      UserInternalWF(userId: 0, name: "Bao Test1", age: 8);
+  UserInternalWF userInternalWF3 =
+      UserInternalWF(userId: 1, name: "Bao Test2", age: 8);
 
-  if (userInternal2 == userInternal2) {
-    //Return false
-  }
-  userInternal1.name = "Change Name";
-  print(userInternal1);
+  //Setter
+  print("\n------------ Test Setter/Getter -----------");
 
-  if (userInternalWF1 == userInternalWF2) {
-    //Return true
-  }
-
-  userInternalWF1.toString();
+  userInternal3.name = "Change userInternal1 Name";
+  print(userInternal3.name);
 
   // userInternalWF1.name = "Change Name";
-  userInternalWF1 = userInternalWF1.copyWith(name: "Change Name");
+  userInternalWF3 =
+      userInternalWF3.copyWith(name: "Change userInternalWF1 Name");
+  print(userInternalWF3.name);
+
+  // compare
+  print("\n------------ Test compare -----------");
+
+  print(
+      "userInternal1 == userInternal2 return ${userInternal1 == userInternal2}");
+
+  print(
+      "userInternalWF1 == userInternalWF2 return ${(userInternalWF1 == userInternalWF2)}");
+  print(
+      "userInternalWF1 == userInternalWF3 return ${(userInternalWF1 == userInternalWF3)}");
+
+  print("\n------------ Test toString -----------");
+
+  userInternal1.toString();
+  print("userInternal1.toString() -> ${userInternal1.toString()}");
+  print("userInternalWF1.toString() -> ${userInternalWF1.toString()}");
 
   runApp(const MyApp());
 }
